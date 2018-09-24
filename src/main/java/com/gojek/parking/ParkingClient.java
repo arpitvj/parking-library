a6ea15edc965477a24e39a56e88370291a10b710
Vijayvargiya <avijayvargiy@c02m242qfh05.agoda.local>
2018-09-24 17:11:56 +0700

    Initial Commit

---
+++ src/main/java/com/gojek/parking/ParkingClient.java
@@ -0,0 +1,14 @@
+package com.gojek.parking;
+
+/**
+ * 
+ * @author avijayvargiy
+ * Parking client
+ */
+public class ParkingClient 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
